80301a440bff6e29dd3591f1d2bf55ae834694af
Nikita Schastnyy <smynay@yandex.ru>
2021-04-20 18:38:54 +0300

    Add pools section

---
+++ src/app/sections/pools-section/pools-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pools-section',
+  templateUrl: './pools-section.component.html',
+  styleUrls: ['./pools-section.component.scss']
+})
+export class PoolsSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
