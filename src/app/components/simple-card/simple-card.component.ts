e18af996a78546299e1ebd8d7377ae5b2304794f
Nikita Schastnyy <smynay@yandex.ru>
2021-04-21 12:03:51 +0300

    Add simple card component

---
+++ src/app/components/simple-card/simple-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-simple-card",
+  templateUrl: "./simple-card.component.html",
+  styleUrls: ["./simple-card.component.scss"],
+})
+export class SimpleCardComponent implements OnInit {
+  @Input() poolInfo = { tokenIco: "assets/images/tokens/BTC.svg" };
+  @Input() decoreImg = "../../../assets/images/decore/2.svg";
+
+  imgStyle = {};
+
+  tokenIco: string;
+
+  constructor() {
+    this.tokenIco = this.poolInfo?.tokenIco;
+    console.log(this.decoreImg);
+  }
+
+  ngOnInit(): void {
+    this.imgStyle = {
+      background: `url('../../../${this.decoreImg}') #fff`,
+      "background-color": "#fff",
+      "background-size": "contain",
+      "background-position": "right 50px",
+      "background-repeat": "no-repeat",
+    };
+  }
+}
