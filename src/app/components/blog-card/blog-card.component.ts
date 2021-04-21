6a30ccd3609f4a8c6594c0a12458c12c14435f5a
Nikita Schastnyy <smynay@yandex.ru>
2021-04-21 18:36:05 +0300

    Add blog card

---
+++ src/app/components/blog-card/blog-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-blog-card",
+  templateUrl: "./blog-card.component.html",
+  styleUrls: ["./blog-card.component.scss"],
+})
+export class BlogCardComponent implements OnInit {
+  @Input() postData = {
+    coverImg: "../../../assets/images/content/2.svg",
+    coverText: "LIKE ME",
+    title: "BE LIKE BE LIKE BE LIKE BE LIKE BE LIKE",
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
