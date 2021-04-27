1fda3c3a5f81747b859815c3da14a8ce57880778
Nikita Schastnyy <smynay@yandex.ru>
2021-04-27 18:36:57 +0300

    Add holdings service integration to altcoins section

--- src/app/sections/altcoins-section/altcoins-section.component.ts
+++ src/app/sections/altcoins-section/altcoins-section.component.ts
@@ -1,21 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+
+import { HoldingsService } from "./../../services/api/holdings.service";
 
 @Component({
-  selector: 'app-altcoins-section',
-  templateUrl: './altcoins-section.component.html',
-  styleUrls: ['./altcoins-section.component.scss']
+  selector: "app-altcoins-section",
+  templateUrl: "./altcoins-section.component.html",
+  styleUrls: ["./altcoins-section.component.scss"],
 })
 export class AltcoinsSectionComponent implements OnInit {
-
-  @Input() poolInfo = { tokenIco: "assets/images/tokens/BTC.svg" };
-
   tokenIco: string;
+  altcoinsInfo: any;
 
-  constructor() {
-    this.tokenIco = this.poolInfo?.tokenIco;
+  constructor(private holdingsService: HoldingsService) {
+    this.tokenIco = "assets/images/tokens/BTC.svg";
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.altcoinsInfo = await this.holdingsService.getUserHoldings();
   }
-
 }
