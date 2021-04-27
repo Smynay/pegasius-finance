329cd719551a7873a3472cd58fc0ad3fe4fdd551
Nikita Schastnyy <smynay@yandex.ru>
2021-04-27 18:13:33 +0300

    Rename to balance service

--- src/app/services/api/balance.service.ts
+++ src/app/services/api/balance.service.ts
@@ -3,28 +3,22 @@ import { Injectable } from "@angular/core";
 @Injectable({
   providedIn: "root",
 })
-export class AltcoinsService {
+export class BalanceService {
   constructor() {}
 
   async getAll() {
     const response = [
       {
-        symbol: "ADA",
+        symbol: "USDC",
         address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-        percentage: 10.52,
+        percentage: 30,
         userholdings: 12521,
       },
       {
         symbol: "WBTC",
         address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-        percentage: 16.47,
+        percentage: 70,
         userholdings: 0.2136734,
-      },
-      {
-        symbol: "LINK",
-        address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-        percentage: 73.01,
-        userholdings: 152.2315,
       },
     ];
 
