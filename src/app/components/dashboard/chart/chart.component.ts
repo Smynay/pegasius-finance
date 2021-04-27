48ebeb2f95b6b9922d3ede741a384b68de25d939
Nikita Schastnyy <smynay@yandex.ru>
2021-04-27 17:16:37 +0300

    Add tvl-history service integration to chrat component

--- src/app/components/dashboard/chart/chart.component.ts
+++ src/app/components/dashboard/chart/chart.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from "@angular/core";
 import { Chart, ChartConfiguration, ChartData, ChartItem, ChartOptions, registerables } from "chart.js";
 Chart.register(...registerables);
 
+import { TvlHistoryService } from "src/app/services/api/tvl-history.service";
+
 @Component({
   selector: "app-chart",
   templateUrl: "./chart.component.html",
@@ -11,74 +13,66 @@ Chart.register(...registerables);
 export class ChartComponent implements OnInit {
   myChart;
 
-  constructor() {}
+  constructor(private tvlHistoryService: TvlHistoryService) {}
 
-  ngOnInit(): void {
-    const DATA_COUNT = 7;
-    const NUMBER_CFG = { count: DATA_COUNT, min: -100, max: 100 };
+  async ngOnInit(): Promise<void> {
+    function getPreparedDataset(dataArray, key) {
+      return {
+        data: dataArray.map((e) => e.tvl),
+        label: key,
+        timestamp: dataArray.map((e) => e.timestamp),
+        clearValue: dataArray.map((e) => e.tvl),
+      };
+    }
 
-    const labels = { may: "May", jul: "Jul", sep: "Sep", nov: "Nov", jan: "Jan" };
-    const data = {
-      type: "line",
-      data: {
-        labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
-        datasets: [
-          {
-            data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
-            label: "Africa",
-            borderColor: "#3e95cd",
-            fill: false,
-          },
-          {
-            data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-            label: "Asia",
-            borderColor: "#8e5ea2",
-            fill: false,
-          },
-          {
-            data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
-            label: "Europe",
-            borderColor: "#3cba9f",
-            fill: false,
-          },
-          {
-            data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
-            label: "Latin America",
-            borderColor: "#e8c3b9",
-            fill: false,
-          },
-          {
-            data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
-            label: "North America",
-            borderColor: "#c45850",
-            fill: false,
-          },
-        ],
-      },
-      options: {
-        title: {
-          display: true,
-          text: "World population per region (in millions)",
-        },
-      },
-    };
+    function getChartLables(dataArray) {
+      function getFullDateValue(value) {
+        const temp = value.toString();
+        return temp.length > 1 ? temp : "0" + temp;
+      }
+
+      const output = [];
+
+      dataArray.forEach((e) => {
+        const date = new Date(+(e.timestamp + "000"));
+        const hour = getFullDateValue(date.getHours());
+        const datestring = `${hour}:00`;
+
+        output.push(datestring);
+      });
+
+      return output;
+    }
+
+    const chartData = await this.tvlHistoryService.getHistory();
+    let labels;
+
+    const datasets = Object.keys(chartData).map((key) => {
+      chartData[key].reverse();
+
+      if (!labels) {
+        labels = getChartLables(chartData[key]);
+      }
+
+      return getPreparedDataset(chartData[key], key);
+    });
+
+    datasets[1].data = datasets[1].data.map((value, index) => value + datasets[0].data[index]);
 
     const config: ChartConfiguration = {
       type: "line",
       data: {
-        labels: ["May", "Jul", "Jun", "Aug", "Sept", "Nov", "Dec"],
+        labels,
         datasets: [
           {
-            data: [500, 200, 245, 105, 304, 180, 279, 221, 450, 300, 1000],
-            label: "WBTC",
+            ...datasets[0],
             borderColor: "#007EAF",
             backgroundColor: "#007EAF",
             tension: 0.4,
             fill: true,
           },
           {
-            data: [282, 350, 411, 502, 400, 371, 586, 320, 201, 170],
-            label: "USDC",
+            ...datasets[1],
             borderColor: "#24126A",
             backgroundColor: "#24126A",
             tension: 0.4,
@@ -95,6 +89,10 @@ export class ChartComponent implements OnInit {
             hoverRadius: 0,
           },
         },
+        interaction: {
+          mode: "index",
+          intersect: false,
+        },
         plugins: {
           legend: {
             position: "bottom",
@@ -107,8 +105,36 @@ export class ChartComponent implements OnInit {
               font: {
                 family: "'Helvetica'",
                 size: 16,
-              }
-            }
+              },
+            },
+          },
+          tooltip: {
+            callbacks: {
+              label: (context: any) => {
+                const tokenName = context.dataset.label;
+                const clearValue = context.dataset.clearValue[context.dataIndex];
+                return `${tokenName}: ${clearValue}`;
+              },
+              title: (context: any) => {
+                function getFullDateValue(value) {
+                  const temp = value.toString();
+                  return temp.length > 1 ? temp : "0" + temp;
+                }
+
+                const newContext = context[0];
+                let title = newContext.label || "";
+
+                const date = new Date(+(newContext.dataset.timestamp[newContext.dataIndex] + "000"));
+                const day = getFullDateValue(date.getDate());
+                const month = getFullDateValue(date.getMonth() + 1);
+
+                if (title) {
+                  title += ` ${day}.${month}`;
+                }
+
+                return title;
+              },
+            },
           },
         },
       },
