ba969759eaa24ac70a90bc5da46c190f9fe527a2
Nikita Schastnyy <smynay@yandex.ru>
2021-04-28 13:04:31 +0300

    Add header back-button props & route detection

--- src/app/components/dashboard/header/header.component.ts
+++ src/app/components/dashboard/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
 import { MenuService } from "src/app/services/menu.service";
 
 @Component({
@@ -15,9 +17,24 @@ export class DashboardHeaderComponent implements OnInit {
     { locale: "FR", title: "France" },
   ];
 
+  backProps = {
+    dashboard: {
+      link: "/",
+      title: "BACK TO HOME PAGE",
+    },
+    pool: {
+      link: "/dashboard",
+      title: "BACK TO DASHBOARD",
+    },
+  };
+
+  page: string;
+
   @Output() burgerClick = new EventEmitter();
 
-  constructor(public menuService: MenuService) {}
+  constructor(public menuService: MenuService, private activatedRoute: ActivatedRoute) {
+    this.page = this.activatedRoute.snapshot.routeConfig.path;
+  }
 
   ngOnInit(): void {}
 
