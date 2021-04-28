2d90d277ff8fcf68ab08f8c078d249efa63403d8
Nikita Schastnyy <smynay@yandex.ru>
2021-04-28 12:07:35 +0300

    Add partners links

--- src/app/sections/partners-section/partners-section.component.ts
+++ src/app/sections/partners-section/partners-section.component.ts
@@ -6,6 +6,27 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./partners-section.component.scss"],
 })
 export class PartnersSectionComponent implements OnInit {
+  partners = [
+    {
+      name: "haechiAudit",
+      darkImg: "../../../assets/images/logo/haechi.svg",
+      lightImg: "../../../assets/images/logo/haechi_light.svg",
+      link: "https://google.com",
+    },
+    {
+      name: "uniswap",
+      darkImg: "../../../assets/images/logo/uniswap.svg",
+      lightImg: "../../../assets/images/logo/uniswap_light.svg",
+      link: "",
+    },
+    {
+      name: "consensys",
+      darkImg: "../../../assets/images/logo/consensys.svg",
+      lightImg: "../../../assets/images/logo/consensys_light.svg",
+      link: "",
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
