e3d14423872d261eed39492f662194821df9e52d
Nikita Schastnyy <smynay@yandex.ru>
2021-05-04 12:26:15 +0300

    Add getFormattedTVL method to pools service

--- src/app/services/api/pools.service.ts
+++ src/app/services/api/pools.service.ts
@@ -5,30 +5,57 @@ import { Pool } from "src/app/models/pool";
   providedIn: "root",
 })
 export class PoolsService {
+  _serviceData = {
+    pools: [
+      {
+        apy: 12.96,
+        "apy-30d": 10.5,
+        symbol: "WETH",
+        asset: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+        tvl: 786,
+        icon: "link-to-logo",
+      },
+      {
+        apy: 37.96,
+        "apy-30d": 40.5,
+        symbol: "USDC",
+        asset: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+        tvl: 815000,
+        icon: "link-to-logo",
+      },
+    ],
+  };
+
+  billion = 1000000000;
+  million = 1000000;
+  thousand = 1000;
+
   constructor() {}
 
   async getAll() {
-    const response = {
-      pools: [
-        {
-          apy: 12.96,
-          "apy-30d": 10.5,
-          symbol: "WETH",
-          asset: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-          tvl: 786,
-          icon: "link-to-logo",
-        },
-        {
-          apy: 37.96,
-          "apy-30d": 40.5,
-          symbol: "USDC",
-          asset: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-          tvl: 815000,
-          icon: "link-to-logo",
-        },
-      ],
-    };
+    return this._serviceData.pools as Pool[];
+  }
 
-    return response.pools as Pool[];
+  async getTVL() {
+    return this._serviceData.pools.reduce((acc, pool) => acc + pool.tvl, 0);
+  }
+
+  async getFormattedTVL(): Promise<string> {
+    const response = await this.getTVL();
+    let newTVL = "$";
+
+    if (response / this.billion > 0.1) {
+      return (newTVL += (response / this.billion).toFixed(1) + "B");
+    }
+
+    if (response / this.million > 0.1) {
+      return (newTVL += (response / this.million).toFixed(1) + "M");
+    }
+
+    if (response / this.thousand > 0.1) {
+      return (newTVL += (response / this.thousand).toFixed(1) + "T");
+    }
+
+    return (newTVL += response.toFixed(1));
   }
 }
