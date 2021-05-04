9b772cea53bbe2bba25ce6f0c770e3afa5431030
Nikita Schastnyy <smynay@yandex.ru>
2021-05-04 11:46:58 +0300

    Fix patners section images quality

--- src/app/sections/partners-section/partners-section.component.ts
+++ src/app/sections/partners-section/partners-section.component.ts
@@ -9,20 +9,20 @@ export class PartnersSectionComponent implements OnInit {
   partners = [
     {
       name: "haechiAudit",
-      darkImg: "../../../assets/images/logo/haechi.svg",
-      lightImg: "../../../assets/images/logo/haechi_light.svg",
+      darkImg: "../../../assets/images/logo/haechi.png",
+      lightImg: "../../../assets/images/logo/haechi_light.png",
       link: "https://google.com",
     },
     {
       name: "uniswap",
-      darkImg: "../../../assets/images/logo/uniswap.svg",
-      lightImg: "../../../assets/images/logo/uniswap_light.svg",
+      darkImg: "../../../assets/images/logo/uniswap.png",
+      lightImg: "../../../assets/images/logo/uniswap_light.png",
       link: "",
     },
     {
       name: "consensys",
-      darkImg: "../../../assets/images/logo/consensys.svg",
-      lightImg: "../../../assets/images/logo/consensys_light.svg",
+      darkImg: "../../../assets/images/logo/consensys.png",
+      lightImg: "../../../assets/images/logo/consensys_light.png",
       link: "",
     },
   ];
