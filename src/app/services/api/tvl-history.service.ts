b9f93da5605f83ad91ecceb8fcc6230665d5eea4
Nikita Schastnyy <smynay@yandex.ru>
2021-05-07 12:56:28 +0300

    Fix tvl history service

--- src/app/services/api/tvl-history.service.ts
+++ src/app/services/api/tvl-history.service.ts
@@ -4,262 +4,244 @@ import { Injectable } from "@angular/core";
   providedIn: "root",
 })
 export class TvlHistoryService {
-  constructor() {}
+  _serviceStore = {
+    WBTC: [
+      {
+        timestamp: 1619388000,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619344800,
+        tvl: 500000,
+      },
+      {
+        timestamp: 1619301600,
+        tvl: 500000,
+      },
+      {
+        timestamp: 1619258400,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619215200,
+        tvl: 800000,
+      },
+      {
+        timestamp: 1619172000,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619128800,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1619085600,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1619042400,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1618999200,
+        tvl: 900000,
+      },
+      {
+        timestamp: 1618956000,
+        tvl: 870000,
+      },
+    ],
+    USDC: [
+      {
+        timestamp: 1619388000,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619344800,
+        tvl: 500000,
+      },
+      {
+        timestamp: 1619301600,
+        tvl: 500000,
+      },
+      {
+        timestamp: 1619258400,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619215200,
+        tvl: 800000,
+      },
+      {
+        timestamp: 1619172000,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619128800,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1619085600,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1619042400,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1618999200,
+        tvl: 900000,
+      },
+      {
+        timestamp: 1618956000,
+        tvl: 870000,
+      },
+    ],
+    WETH: [
+      {
+        timestamp: 1619388000,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619344800,
+        tvl: 500000,
+      },
+      {
+        timestamp: 1619301600,
+        tvl: 500000,
+      },
+      {
+        timestamp: 1619258400,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619215200,
+        tvl: 800000,
+      },
+      {
+        timestamp: 1619172000,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619128800,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1619085600,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1619042400,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1618999200,
+        tvl: 900000,
+      },
+      {
+        timestamp: 1618956000,
+        tvl: 870000,
+      },
+    ],
+    VITYA: [
+      {
+        timestamp: 1619388000,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619344800,
+        tvl: 500000,
+      },
+      {
+        timestamp: 1619301600,
+        tvl: 500000,
+      },
+      {
+        timestamp: 1619258400,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619215200,
+        tvl: 800000,
+      },
+      {
+        timestamp: 1619172000,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619128800,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1619085600,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1619042400,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1618999200,
+        tvl: 900000,
+      },
+      {
+        timestamp: 1618956000,
+        tvl: 870000,
+      },
+    ],
+    NIKITAS: [
+      {
+        timestamp: 1619388000,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619344800,
+        tvl: 500000,
+      },
+      {
+        timestamp: 1619301600,
+        tvl: 500000,
+      },
+      {
+        timestamp: 1619258400,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619215200,
+        tvl: 800000,
+      },
+      {
+        timestamp: 1619172000,
+        tvl: 700000,
+      },
+      {
+        timestamp: 1619128800,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1619085600,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1619042400,
+        tvl: 870000,
+      },
+      {
+        timestamp: 1618999200,
+        tvl: 900000,
+      },
+      {
+        timestamp: 1618956000,
+        tvl: 870000,
+      },
+    ],
+  };
 
   async getHistory() {
-    const response = {
-      ETH: [
-        {
-          timestamp: 1619388000,
-          tvl: 700000,
-        },
-        {
-          timestamp: 1619344800,
-          tvl: 500000,
-        },
-        {
-          timestamp: 1619301600,
-          tvl: 500000,
-        },
-        {
-          timestamp: 1619258400,
-          tvl: 700000,
-        },
-        {
-          timestamp: 1619215200,
-          tvl: 800000,
-        },
-        {
-          timestamp: 1619344800,
-          tvl: 700000,
-        },
-        {
-          timestamp: 1619172000,
-          tvl: 700000,
-        },
-        {
-          timestamp: 1619128800,
-          tvl: 870000,
-        },
-        {
-          timestamp: 1619085600,
-          tvl: 870000,
-        },
-        {
-          timestamp: 1619042400,
-          tvl: 870000,
-        },
-        {
-          timestamp: 1618999200,
-          tvl: 900000,
-        },
-        {
-          timestamp: 1618956000,
-          tvl: 870000,
-        },
-      ],
-      USDC: [
-        {
-          timestamp: 1619388000,
-          tvl: 674000,
-        },
-        {
-          timestamp: 1619344800,
-          tvl: 612000,
-        },
-        {
-          timestamp: 1619301600,
-          tvl: 664000,
-        },
-        {
-          timestamp: 1619258400,
-          tvl: 656000,
-        },
-        {
-          timestamp: 1619215200,
-          tvl: 715000,
-        },
-        {
-          timestamp: 1619344800,
-          tvl: 769000,
-        },
-        {
-          timestamp: 1619172000,
-          tvl: 835000,
-        },
-        {
-          timestamp: 1619128800,
-          tvl: 841000,
-        },
-        {
-          timestamp: 1619085600,
-          tvl: 841000,
-        },
-        {
-          timestamp: 1619042400,
-          tvl: 864000,
-        },
-        {
-          timestamp: 1618999200,
-          tvl: 600000,
-        },
-        {
-          timestamp: 1618956000,
-          tvl: 815000,
-        },
-      ],
-      WETH: [
-        {
-          timestamp: 1619388000,
-          tvl: 679000,
-        },
-        {
-          timestamp: 1619344800,
-          tvl: 856000,
-        },
-        {
-          timestamp: 1619301600,
-          tvl: 110002,
-        },
-        {
-          timestamp: 1619258400,
-          tvl: 900100,
-        },
-        {
-          timestamp: 1619215200,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619344800,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619172000,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619128800,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619085600,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619042400,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1618999200,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1618956000,
-          tvl: 856422,
-        },
-      ],
-      VITYA: [
-        {
-          timestamp: 1619388000,
-          tvl: 679000,
-        },
-        {
-          timestamp: 1619344800,
-          tvl: 856000,
-        },
-        {
-          timestamp: 1619301600,
-          tvl: 110002,
-        },
-        {
-          timestamp: 1619258400,
-          tvl: 900100,
-        },
-        {
-          timestamp: 1619215200,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619344800,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619172000,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619128800,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619085600,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619042400,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1618999200,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1618956000,
-          tvl: 856422,
-        },
-      ],
-      NIKITAS: [
-        {
-          timestamp: 1619388000,
-          tvl: 679000,
-        },
-        {
-          timestamp: 1619344800,
-          tvl: 856000,
-        },
-        {
-          timestamp: 1619301600,
-          tvl: 110002,
-        },
-        {
-          timestamp: 1619258400,
-          tvl: 900100,
-        },
-        {
-          timestamp: 1619215200,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619344800,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619172000,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619128800,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619085600,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1619042400,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1618999200,
-          tvl: 856422,
-        },
-        {
-          timestamp: 1618956000,
-          tvl: 856422,
-        },
-      ],
-    };
+    return { ...this._serviceStore };
+  }
 
-    return response;
+  async getHistoryBySymbol(tokenSymbol: string) {
+    return { [tokenSymbol]: this._serviceStore[tokenSymbol] };
   }
 }
